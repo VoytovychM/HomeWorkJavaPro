5c1ffae7e06b3257f3616292072a12ad72583e9f
VoytovychM <mary.voytovych@gmail.com>
2024-02-27 23:13:28 +0100

    Homework No2 add mouse class

---
+++ src/lesson20240219/house/Mouse.java
@@ -0,0 +1,13 @@
+package lesson20240219.house;
+
+public class Mouse extends Creature {
+
+    public Mouse(String name) {
+        super(name);
+    }
+
+    @Override
+    public void sayHello() {
+
+    }
+}
