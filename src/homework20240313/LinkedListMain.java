4deff441048fa9d2ccc2f0088dd66024d80bb3a8
VoytovychM <mary.voytovych@gmail.com>
2024-03-18 18:28:41 +0100

    homework dd 13.03.2024

---
+++ src/homework20240313/LinkedListMain.java
@@ -0,0 +1,80 @@
+package homework20240313;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+
+         //TaskNo1
+        List<String> list = new LinkedList<>();
+        list.add ("Elephant");
+        list.add("Cat");
+        list.add ("Fish");
+
+        System.out.println(list);
+
+        String shortestWord = null;
+        for (String animal: list){
+          if( shortestWord == null || shortestWord.length() > animal.length()) {
+              shortestWord = animal;
+          }
+        }
+        System.out.println("SHORTEST WORD: " + shortestWord);
+
+
+        //TaskNo2
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String animal: list){
+            stringBuilder.append(animal);
+            stringBuilder.append("|");
+        }
+        System.out.println(stringBuilder);
+
+        //TaskNo3
+        List<TestClass> listClass = new LinkedList<>();
+        listClass.add (new TestClass("Elephant"));
+        listClass.add(new TestClass("Cat"));
+        listClass.add (new TestClass("Fish"));
+
+        for(TestClass testClass: listClass){
+            if(testClass.getField().length() <= 3){
+                listClass.remove(testClass);
+            }
+        }
+        System.out.println(listClass);
+
+        //TaskNo4
+
+       LinkedList<Integer> integerList = new LinkedList<>();
+       integerList.add(1);
+       integerList.add(4);
+       integerList.add(3);
+       integerList.add(2);
+       integerList.add(1);
+       integerList.add(2);
+       integerList.add(2);
+       integerList.add(0);
+       System.out.println(integerList);
+
+       int element = 2;
+       int[] result = findFirstAndLastIndex(integerList, element);
+        System.out.println("First index: " + result[0]);
+        System.out.println("Last index: " + result[1]);
+
+    }
+       public static int[] findFirstAndLastIndex (LinkedList<Integer> integerList, int element){
+        int firstIndex = -1;
+        int lastIndex = -1;
+           for (int i = 0; i < integerList.size(); i++) {
+               if (integerList.get(i) == element){
+                   if (firstIndex == -1){
+                       firstIndex =i;
+                   }
+               lastIndex =i;
+           }
+       }
+           return new int[]{firstIndex, lastIndex};
+    }
+
+}
---
+++ src/homework20240313/TestClass.java
@@ -0,0 +1,25 @@
+package homework20240313;
+
+public class TestClass {
+
+    String field;
+
+    public TestClass(String field) {
+        this.field = field;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    @Override
+    public String toString() {
+        return "TestClass{" +
+                "field='" + field + '\'' +
+                '}';
+    }
+}
