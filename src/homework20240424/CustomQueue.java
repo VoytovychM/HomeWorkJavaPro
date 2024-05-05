e1aa26f1b709917f912ff8b63e541c26d30c012f
VoytovychM <mary.voytovych@gmail.com>
2024-05-05 22:38:12 +0200

    hw  20240424

--- src/homework20240424/CustomQueue.java
+++ src/homework20240424/CustomQueue.java
@@ -1,7 +1,42 @@
 package homework20240424;
-//Переделать класс CustomQueue (пакет lesson20240325.queue) так, чтобы этот класс мог хранить данные любого типа
-public class CustomQueue {
-    public static void main(String[] args) {
 
+import java.util.ArrayDeque;
+
+//Переделать класс CustomQueue (пакет lesson20240325.queue) так, чтобы этот класс мог хранить данные любого типа
+public class CustomQueue<T> {
+
+    private ArrayDeque<T> deque = new ArrayDeque<>();
+
+
+    public void enqueue(T data){
+        deque.add(data);
     }
+
+    public T dequeue(){
+        return deque.removeFirst();
+    }
+
+    public static void main(String[] args) {
+        CustomQueue<String> myQueue = new CustomQueue<>();
+        myQueue.enqueue("A");
+        myQueue.enqueue("B");
+        myQueue.enqueue("C");
+
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.dequeue());
+
+
+        CustomQueue<Integer> myQueueInt = new CustomQueue<>();
+        myQueueInt.enqueue(1);
+        myQueueInt.enqueue(2);
+        myQueueInt.enqueue(3);
+
+        System.out.println(myQueueInt.dequeue());
+        System.out.println(myQueueInt.dequeue());
+        System.out.println(myQueueInt.dequeue());
+    }
+
+
 }
+
--- src/homework20240424/ReserveOrder.java
+++ src/homework20240424/ReserveOrder.java
@@ -1,7 +1,36 @@
 package homework20240424;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 //Написать метод, который принимает список любого типа и возвращает его в виде нового списка с элементами в обратном порядке.
 public class ReserveOrder {
+
+    public static  List<?> reverseOrder (List<?> input){
+        List<?> reverseList = new ArrayList<>(input);
+        Collections.reverse(reverseList);
+        return reverseList;
+    }
     public static void main(String[] args) {
+
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Love");
+        stringList.add("Respect");
+        stringList.add("Honesty");
+        stringList.add("Justice");
+
+        System.out.println(stringList);
+        System.out.println(reverseOrder(stringList));
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(123);
+        integerList.add(456);
+        integerList.add(789);
+        integerList.add(1011);
+        System.out.println(integerList);
+        System.out.println(reverseOrder(integerList));
 
     }
 }
