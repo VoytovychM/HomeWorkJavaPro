00bce37b6b43f3d735f800bbe49d5180eeb23c7f
VoytovychM <mary.voytovych@gmail.com>
2024-05-23 21:42:47 +0200

    hw20240522

---
+++ src/homework20240522/ThreadTask.java
@@ -0,0 +1,59 @@
+package homework20240522;
+
+public class ThreadTask {
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+
+        int number = 5;
+        int count = 0;
+
+        RangeCalculater rangeCalculater1 = new RangeCalculater(Integer.MIN_VALUE, 0, number);
+        RangeCalculater rangeCalculater2 = new RangeCalculater(0, Integer.MAX_VALUE, number);
+        // System.out.println(Integer.MIN_VALUE + " " + " " + Integer.MAX_VALUE); -2147483648  2147483647
+        rangeCalculater1.start();
+        rangeCalculater2.start();
+
+
+        try {
+            rangeCalculater1.join();
+            rangeCalculater2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        count = rangeCalculater1.getCount() + rangeCalculater2.getCount();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Total divisible by " + number + ":" + count);
+        System.out.println("Time elapsed: " + (end - start) + ", ms");
+    }
+
+    static class RangeCalculater extends Thread{
+
+        int start;
+        int end;
+        int count =0;
+        int divider;
+
+        public RangeCalculater(int start, int end, int divider) {
+            this.start = start;
+            this.end = end;
+            this.divider = divider;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @Override
+        public void run() {
+            for(int i = start; i < end; i++){
+                if(i % divider == 0)
+                    count ++;
+            }
+
+        }
+    }
+}
